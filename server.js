90ec502a51a26506c5ae2683d9a98b1c509c60bd
Jamal <jamalmizyed08@gmail.com>
2023-11-07 17:53:22 -0600

    working rapper api

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const PORT = 8000
+
+const rappers = {
+    '21 savage': {
+        'birthName': 'Sheyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England',
+        'age': 29
+    },
+    'chance the rapper': {
+        'birthName': 'Chancelor Bennett',
+        'birthLocation': 'Chicago, Illinois',
+        'age': 29
+    },
+    'unknown': {
+        'birthName': 'unknown',
+        'birthLocation': 'unknown',
+        'age': 0
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')
+})
+
+app.get('/api/:name', (request, response) => {
+    //request.params.name is anything that comes after the /
+    let rapperName = request.params.name.toLowerCase()
+    if(rappers[rapperName]) {
+        response.json(rappers[rapperName])
+    } else {
+        response.json(rappers['unknown'])
+    }
+    response.json(rappers)
+})
+
+app.listen(PORT, () => {
+    console.log(`The server is now running on port ${PORT}, better go catch it!`)
+})
